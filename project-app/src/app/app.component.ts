3c0a9b45bccabd905d47a7658c328b8af4252a6a
Rodrigo Dominguez <rorra@rorra.com.ar>
2018-12-11 02:32:57 -0300

    Section 20

--- project-app/src/app/app.component.ts
+++ project-app/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAJ6Akz-RSD9ho6VZ_mPKY_yyYtoRKlpXw",
+      authDomain: "rorra-udemy-ng-http.firebaseapp.com"
+    });
+  }
 
   onNavigate(feature: string): void {
     this.loadedFeature = feature;
--- project-app/src/app/shared/data-storage.service.ts
+++ project-app/src/app/shared/data-storage.service.ts
@@ -3,18 +3,25 @@ import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { map } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService) {
+  constructor(private http: Http,
+              private recipeService: RecipeService,
+              private authService: AuthService) {
   }
 
   storeRecipes() {
-    return this.http.put('https://rorra-udemy-ng-http.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put('https://rorra-udemy-ng-http.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    return this.http.get('https://rorra-udemy-ng-http.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+
+    return this.http.get('https://rorra-udemy-ng-http.firebaseio.com/recipes.json?auth=' + token)
       .pipe(map((response: Response) => {
         const recipes: Recipe[] = response.json();
         for (const recipe of recipes) {
@@ -22,7 +29,7 @@ export class DataStorageService {
             recipe['ingredients'] = [];
           }
         }
-        return(recipes);
+        return (recipes);
       })).subscribe(
         (recipes: Recipe[]) => {
           this.recipeService.setRecipes(recipes);
