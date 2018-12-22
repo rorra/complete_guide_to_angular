51e2c0aa7214f96ba859a5fdbe48c0cfaf22bf34
Rodrigo Dominguez <rorra@rorra.com.ar>
2018-12-22 01:47:44 -0300

    Section 23

--- project-app/src/app/shared/data-storage.service.ts
+++ project-app/src/app/shared/data-storage.service.ts
@@ -1,29 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http,
+  constructor(private httpClient: HttpClient,
               private recipeService: RecipeService,
               private authService: AuthService) {
   }
 
   storeRecipes() {
-    const token = this.authService.getToken();
-
-    return this.http.put('https://rorra-udemy-ng-http.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    return this.httpClient.put('https://rorra-udemy-ng-http.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes(),
+      {
+        observe: 'body'
+      }
+    );
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
-
-    return this.http.get('https://rorra-udemy-ng-http.firebaseio.com/recipes.json?auth=' + token)
-      .pipe(map((response: Response) => {
-        const recipes: Recipe[] = response.json();
+    return this.httpClient.get<Recipe[]>('https://rorra-udemy-ng-http.firebaseio.com/recipes.json')
+      .pipe(map((recipes) => {
         for (const recipe of recipes) {
           if (recipe['ingredients']) {
             recipe['ingredients'] = [];
